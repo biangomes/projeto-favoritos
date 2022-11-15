7ea866bfdb24cd0cdd2cfa1311c710dd50f18d48
biangomes <biangomes0707@gmail.com>
2022-11-14 20:43:04 -0500

    Package.json e inclusao do nodemon

--- server.js
+++ server.js
@@ -5,17 +5,29 @@ const fs = require('fs')            // permite trabalhar com arquivos
 
 http.createServer((req, resp) => {
 
+
+    const fileHtml = req.url === '/' ? 'index.html' : req.url
+    // substitui o path.join(...)
+    const filePath = path.join(__dirname, 'public', fileHtml)
+  
+    /*
+    //pegando a extensãp do documento
+    const extName = path.extname(filePath)
+
+    //criando um vetor com as extensões permitidas
+    const allowedFileTypes = ['.html', '.css', '.js']
+
+    const allowed = allowedFileTypes.find(
+        (item) => item === extName
+    )
+
+    if (!allowed) return 
+*/
+
     // definindo rotas para a aplicação
-    if (req.url === '/')
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
-            (err, content) => {
-                if (err) throw err 
-                    resp.end(content)
-        })
-    if (req.url === '/contato') {
-        resp.end("<h1>contact</h1>")
-    }
-}).listen(5000, () => {
-    console.log("testando texto no console da aplicação ... ")
-});
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err 
+                resp.end(content)
+    })}).listen(5000, () => {console.log("testando texto no console da aplicação ... ")});
